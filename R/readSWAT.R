93170a464b9ec049b502a4f724e776a0e7ae6d48
jsta <stachel2@msu.edu>
2018-02-04 12:04:16 -0500

    start making flexible file paths

--- R/readSWAT.R
+++ R/readSWAT.R
@@ -1,15 +1,15 @@
 #' Read output.* files from SWAT200*
-#' 
+#'
 #' A function to read output.* files from SWAT200* runs.
-#' 
-#' 
+#'
+#'
 #' @param outfile_type outfile_type ie. "sub","rch",etc.
 #' @author Daniel R. Fuka
 #' @examples
-#' 
+#'
 #' ## The function is currently defined as
 #' function(outfile){
-#' 
+#'
 #' if (outfile=="sub"){
 #'    varformat="x6,a4,1x,a8,1x,a4,a10,30a10"
 #'    dataformat="x6,i4,1x,i8,1x,i4,f10,30f10"
@@ -25,21 +25,29 @@
 #'   outdata=read.fortran(paste("output.",outfile,sep=""),dfrformat,skip=9,col.names=outvars)
 #'   return(outdata)
 #'   }
-#' 
-readSWAT <-
-function(outfile_type){
-if(missing(outfile_type)){print(" 'outfile_type' is missing, should be rch, sub, or.. ask drf28 for other types")}
-if (outfile_type=="sub"){
-   varformat="x6,a4,1x,a8,1x,a4,a10,30a10"
-   dataformat="x6,i4,1x,i8,1x,i4,f10,30f10"
-} else if (outfile_type=="rch"){
-   varformat="x6,a4,1x,a8,1x,a5,30a12"
-   dataformat="x6,i4,1x,i8,1x,i5,30f12"
-} else { print ("You need to add your file type to this function if it is not output.sub or output.rch")}
-  vfrformat = unlist(strsplit(as.character(varformat), ","))
-  dfrformat = unlist(strsplit(as.character(dataformat), ","))
-  outvars=read.fortran(paste("output.",outfile_type,sep=""),vfrformat,skip=8,nrows=1)
-  outdata=read.fortran(paste("output.",outfile_type,sep=""),dfrformat,skip=9,col.names=outvars)
+#'
+readSWAT <- function(outfile_path = "output", outfile_type){
+if(missing(outfile_type)){
+  stop(
+    " 'outfile_type' is missing, should be rch, sub")
+  }
+  if(outfile_type == "sub") {
+    varformat  <- "x6,a4,1x,a8,1x,a4,a10,30a10"
+    dataformat <- "x6,i4,1x,i8,1x,i4,f10,30f10"
+  }else{
+    if (outfile_type == "rch") {
+      varformat  <- "x6,a4,1x,a8,1x,a5,30a12"
+      dataformat <- "x6,i4,1x,i8,1x,i5,30f12"
+    }else{
+      stop("You need to add your file type to this function if it is not .sub or .rch")
+    }
+  }
+  vfrformat <- unlist(strsplit(as.character(varformat), ","))
+  dfrformat <- unlist(strsplit(as.character(dataformat), ","))
+
+  outvars   <- read.fortran(paste0(outfile_path, ".", outfile_type), vfrformat,
+                          skip = 8, nrows = 1)
+  outdata   <- read.fortran(paste0(outfile_path, ".", outfile_type), dfrformat,
+                          skip = 9, col.names = outvars)
   return(outdata)
 }
-
--- R/runSWAT2012.R
+++ R/runSWAT2012.R
@@ -1,39 +1,54 @@
 #' This runs the SWAT2012 executable in the current directory.
-#' 
+#'
 #' This function runs the SWAT2012 executable in the current directory.
-#' 
-#' 
+#'
+#'
 #' @param hist_wx Describe \code{hist_wx}
 #' @param elev Describe \code{elev}
 #' @param rch Describe \code{rch}
 #' @author Daniel R. Fuka
-runSWAT2012<-function (hist_wx=NULL,elev=100,rch=3) {
-    Sys.setenv(GFORTRAN_STDIN_UNIT = -1)
-    Sys.setenv(GFORTRAN_STDOUT_UNIT = -1)
-    Sys.setenv(GFORTRAN_STDERR_UNIT = -1)
-    if(length(hist_wx)>2){
-      tmp_head = paste("Tmp\nLati Not Used\nLong Not Used\nElev        ", sprintf("%5.0f\n", elev), sep = "")
-      pcp_head = paste("Pcp\nLati Not Used\nLong Not Used\nElev        ", sprintf("%5.0f\n", elev), sep = "")
-      cat(tmp_head, sprintf("%s%005.1f%005.1f\n", format(hist_wx$DATE, "%Y%j"), hist_wx$TMX, hist_wx$TMN), file = "tmp.tmp", sep = "")
-      cat(pcp_head, sprintf("%s%005.1f\n", format(hist_wx$DATE, "%Y%j"), hist_wx$PRECIP), file = "pcp.pcp", sep = "")
-      print("built new pcp.pcp and tmp.tmp files, make sure they are correct in file.cio")
-    }
-    libarch = if (nzchar(version$arch)) paste("libs", version$arch, sep = "/") else "libs"
-    swatbin <- "rswat2012.exe"
-    system(shQuote(paste(path.package("SWATmodel"),libarch,swatbin, sep = "/")))
+#'
+#' @examples \dontrun{
+#' runSWAT2012("test")
+#' }
+runSWAT2012 <- function(simdir = ".", hist_wx = NULL, elev = 100, rch = 3) {
+  Sys.setenv(GFORTRAN_STDIN_UNIT = -1)
+  Sys.setenv(GFORTRAN_STDOUT_UNIT = -1)
+  Sys.setenv(GFORTRAN_STDERR_UNIT = -1)
+  if(length(hist_wx) > 2) {
+    tmp_head <- paste("Tmp\nLati Not Used\nLong Not Used\nElev        ",
+                      sprintf("%5.0f\n", elev), sep = "")
+    pcp_head <- paste("Pcp\nLati Not Used\nLong Not Used\nElev        ",
+                      sprintf("%5.0f\n", elev), sep = "")
+    cat(tmp_head, sprintf("%s%005.1f%005.1f\n", format(hist_wx$DATE,
+                                                       "%Y%j"), hist_wx$TMX, hist_wx$TMN), file = "tmp.tmp", sep = "")
+    cat(pcp_head, sprintf("%s%005.1f\n", format(hist_wx$DATE, "%Y%j"),
+                          hist_wx$PRECIP), file = "pcp.pcp", sep = "")
+    print("built new pcp.pcp and tmp.tmp files, make sure they are correct in file.cio")
+  }
 
-    start_year = read.fortran(textConnection(readLines("file.cio")[9]), "f20")
-    temp = readLines(file("output.rch"))
-    rchcolname = sub(" ", "", (substr(temp[9], 50, 61)))
-    flow = data.frame(as.numeric(as.character(substr(temp[10:length(temp)], 50, 61))))
-    colnames(flow) = rchcolname
-    reach = data.frame(as.numeric(as.character(substr(temp[10:length(temp)], 8, 10))))
-    rchcolname = sub(" ", "", (substr(temp[9], 8, 10)))
-    colnames(reach) = rchcolname
-    outdata = cbind(reach, flow)
-    temp2 = subset(outdata, outdata$RCH == rch)
-    temp2$mdate = as.Date(row(temp2)[, 1], origin = paste(start_year - 1, "-12-31", sep = ""))
-    return(temp2)
+  libarch <- if (nzchar(version$arch))
+    paste("libs", version$arch, sep = "/") else "libs"
+  swatbin <- "rswat2012.exe"
+  system(shQuote(paste(path.package("swatr"), libarch, swatbin, sep = "/")))
+
+  start_year <- read.fortran(textConnection(readLines(
+    file.path(simdir, "file.cio"))[9]), "f20")
+  browser()
+  temp <- readLines(file("output.rch"))
+  rchcolname <- sub(" ", "", (substr(temp[9], 50, 61)))
+  flow <- data.frame(as.numeric(as.character(substr(temp[10:length(temp)],
+                                                    50, 61))))
+  colnames(flow) <- rchcolname
+  reach <- data.frame(as.numeric(as.character(substr(temp[10:length(temp)],
+                                                     8, 10))))
+  rchcolname <- sub(" ", "", (substr(temp[9], 8, 10)))
+  colnames(reach) <- rchcolname
+  outdata <- cbind(reach, flow)
+  temp2 <- subset(outdata, outdata$RCH == rch)
+  temp2$mdate <- as.Date(row(temp2)[, 1], origin = paste(start_year -
+                                                           1, "-12-31", sep = ""))
+  return(temp2)
 
 }
 
